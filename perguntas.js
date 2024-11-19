7bdc0dd6778b954a86bf92bf94cedbad5648fdb4
LebronJames194 <00001099263220sp@al.educacao.sp.gov.br>
2024-11-19 11:30:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é que tem quatro pernas por fora, dois braços e duas pernas por dentro?',
+    'O cachorro do goleiro Bruno'
+)
+
+criaCartao(
+    'Geografia',
+    'onde fica الورا دا کاروبار?',
+    'fica em د کاوبار'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma tarefa de casa?',
+    'Uma coisa para se ignorar'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "eu fantasma baixo legal"?',
+    'I ghost down cool.'
+)
